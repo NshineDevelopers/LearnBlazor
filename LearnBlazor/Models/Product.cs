87b25192c661bebd961ca6cb908853da07bfe3a9
Mohammed Mubashir <mubashir@nshine.tech>
2024-09-18 10:49:56 +0530

    Add project files.

---
+++ LearnBlazor/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace LearnBlazor.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; } = 0;
+        public bool IsActive { get; set; } = false;
+        public IEnumerable<Product_Prop> ProductProperties { get; set; }
+    }
+}
